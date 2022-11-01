d1eccfdac51cfcf4b1b8eeeb7be9838cdaa9170d
phandinh94 <phanthidinh58@gmail.com>
2022-11-01 16:50:47 +0700

    add 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -12,49 +12,52 @@ module.exports.getAll = (req, res) => {
       console.log(err);
     });
 };
-module.exports.login = (req, res) => {
-  // console.log(req.body);
-  let { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(500).json({
-      message: "Invalid email or password",
-    });
-  }
-  db.execute("SELECT * FROM new_table WHERE email=?", [email])
-    .then((data) => {
-      console.log(data);
-      let [rows] = data;
-      let find = rows[0];
-      if (!find) {
-        res.status(404).json({
-          message: "user is not exist",
-        });
-      } else {
-        //check password
-        console.log(find.password);
-        let passwordValid = bcrypt.compareSync(password, find.password);
-        if (!passwordValid) {
-          res.status(404).json({
-            message: "Wrong password",
-          });
-        } else {
-          console.log("Hello");
-          // res.cookie("userId", find.id, { signed: true });
-          // res.cookie("role", find.role, { signed: true });
+module.exports.userLogin = (req, res) => {
+  res.render("login")
+}
+// module.exports.login = (req, res) => {
+//   // console.log(req.body);
+//   let { email, password } = req.body;
+//   if (!email || !password) {
+//     return res.status(500).json({
+//       message: "Invalid email or password",
+//     });
+//   }
+//   db.execute("SELECT * FROM new_table WHERE email=?", [email])
+//     .then((data) => {
+//       console.log(data);
+//       let [rows] = data;
+//       let find = rows[0];
+//       if (!find) {
+//         res.status(404).json({
+//           message: "user is not exist",
+//         });
+//       } else {
+//         //check password
+//         console.log(find.password);
+//         let passwordValid = bcrypt.compareSync(password, find.password);
+//         if (!passwordValid) {
+//           res.status(404).json({
+//             message: "Wrong password",
+//           });
+//         } else {
+//           console.log("Hello");
+//           // res.cookie("userId", find.id, { signed: true });
+//           // res.cookie("role", find.role, { signed: true });
 
-          console.log("Hello");
-          res.status(200).json({
-            status: "success",
-            message: "Login successfully",
-          });
-          // điều hướng người dùng sang trang "/"
-          // set header
-          // res.redirect // not working after set cookie
-          // res.redirect not working after res.cookie (google)
-        }
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+//           console.log("Hello");
+//           res.status(200).json({
+//             status: "success",
+//             message: "Login successfully",
+//           });
+//           // điều hướng người dùng sang trang "/"
+//           // set header
+//           // res.redirect // not working after set cookie
+//           // res.redirect not working after res.cookie (google)
+//         }
+//       }
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
